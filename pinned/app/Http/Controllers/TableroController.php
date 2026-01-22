7ca82617289375e22ca440a52ed6d47a6720fa79
lsallop <lsallop3012@g.educaand.es>
2026-01-22 18:04:43 +0100

    TFG-Creación de los modelos  del proyecto

---
+++ pinned/app/Http/Controllers/TableroController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tablero;
+use Illuminate\Http\Request;
+
+class TableroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tablero $tablero)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tablero $tablero)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tablero $tablero)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tablero $tablero)
+    {
+        //
+    }
+}
